684586bda7d2646f00ce31980403291934663b2c
orangemiracle <orangemarvel0401@gmail.com>
2023-03-28 03:19:21 -0400

    添加项目文件。

---
+++ MVC Agile Process with Agile Work Item Process/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_Agile_Process_with_Agile_Work_Item_Process.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
